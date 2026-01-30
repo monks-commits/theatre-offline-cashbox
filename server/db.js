e3943b68aed325f96425bd8de9f24930e64a2a09
monks-commits <monks@mail.ru>
2026-01-30 11:56:04 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,15 @@
+import Database from "better-sqlite3";
+import fs from "fs";
 
+const DB_FILE = "./server/db.sqlite";
+
+const firstRun = !fs.existsSync(DB_FILE);
+const db = new Database(DB_FILE);
+
+if (firstRun) {
+  const schema = fs.readFileSync("./server/schema.sql", "utf8");
+  db.exec(schema);
+  console.log("DB initialized");
+}
+
+export default db;
